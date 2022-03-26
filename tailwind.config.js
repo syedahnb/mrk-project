67a1389adaa0e02815a045a048c7b566b2c0ddaa
Syed Arslan Haider <sahnb@outlook.com>
2022-03-26 09:21:02 +0500

    Basic Home Page Setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
